5d736d9212725fffbda2d9e0236b2c3c68ca00d6
Matthew Armstrong <matthew.armstrong@andgosystems.com>
2024-03-07 21:32:07 -0600

    double pong

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
-use std::io::Write;
-use std::net::{TcpListener, TcpStream};
-
-fn handle_ping(mut stream: TcpStream) -> anyhow::Result<()> {
-    const PONG: &'static str = "+PONG\r\n";
-    stream.write(PONG.as_bytes())?;
-    stream.write(PONG.as_bytes())?;
-    Ok(())
-}
+use std::io::{Read, Write};
+use std::net::TcpListener;
 
 fn main() {
+    const PONG: &'static str = "+PONG\r\n";
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(stream) => handle_ping(stream).unwrap(),
+            Ok(mut stream) => {
+                let mut buffer = [0; 1024];
+                while let Ok(val) = stream.read(&mut buffer) {
+                    if val != 0 {
+                        stream.write(PONG.as_bytes()).unwrap();
+                    }
+                }
+            }
             Err(e) => {
                 println!("error: {}", e);
             }
