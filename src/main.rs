4b3ec0b0dc111cb9afa7e25d2b1de853e75438e1
Matthew Armstrong <matthew.armstrong@andgosystems.com>
2024-03-07 21:20:47 -0600

    pong

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
-use std::net::TcpListener;
+use std::io::Write;
+use std::net::{TcpListener, TcpStream};
+
+fn handle_ping(mut stream: TcpStream) -> anyhow::Result<()> {
+    const PONG: &'static str = "+PONG\r\n";
+    stream.write(PONG.as_bytes())?;
+    Ok(())
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-            }
+            Ok(stream) => handle_ping(stream).unwrap(),
             Err(e) => {
                 println!("error: {}", e);
             }
