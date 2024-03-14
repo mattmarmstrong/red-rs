975184da68a991b1fef312ab245aa815d04d530e
Matthew Armstrong <matthew.armstrong@andgosystems.com>
2024-03-13 22:21:30 -0600

    ok that one is one me

--- src/server/replicate.rs
+++ src/server/replicate.rs
@@ -1,7 +1,7 @@
 use rand::distributions::Alphanumeric;
 use rand::{thread_rng, Rng};
 
-fn gen_master_repl_id() -> String {
+fn gen_master_replid() -> String {
     thread_rng()
         .sample_iter(&Alphanumeric)
         .take(40)
@@ -53,22 +53,22 @@ impl ReplicaInfo {
     }
 
     pub fn default() -> Self {
-        Self::new(Role::Master, 0, gen_master_repl_id(), 0)
+        Self::new(Role::Master, 0, gen_master_replid(), 0)
     }
 
     // fake it till you make it
     pub fn fake_replica() -> Self {
-        Self::new(Role::Slave, 0, gen_master_repl_id(), 0)
+        Self::new(Role::Slave, 0, gen_master_replid(), 0)
     }
 
-    pub fn replica(master_repl_id: String, master_repl_offset: usize) -> Self {
-        Self::new(Role::Slave, 0, master_repl_id, master_repl_offset)
+    pub fn replica(master_replid: String, master_repl_offset: usize) -> Self {
+        Self::new(Role::Slave, 0, master_replid, master_repl_offset)
     }
 
     pub fn to_string(&self) -> String {
         let role = format("role", self.role.to_str());
-        let master_repl_id = format("master_replid", &self.master_repl_id);
+        let master_replid = format("master_replid", &self.master_replid);
         let master_repl_offset = format("master_repl_offset", &self.master_repl_offset.to_string());
-        [&role, "\r\n", &master_repl_id, "\r\n", &master_repl_offset].concat()
+        [&role, "\r\n", &master_replid, "\r\n", &master_repl_offset].concat()
     }
 }
