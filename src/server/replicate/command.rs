3ba13ed2f7a41618cb245c71ccc9c439acc0127e
Matthew Armstrong <matthew.armstrong@andgosystems.com>
2024-03-15 17:52:36 -0600

    Revert "what could it possibly be"
    This reverts commit 4879fca7e14b94453c7b111dcf5424d561e839bf.

--- src/server/replicate/command.rs
+++ src/server/replicate/command.rs
@@ -3,6 +3,7 @@ use std::net::TcpStream;
 
 use crate::resp::parse::Parser;
 use crate::resp::serialize::Serializer;
+use crate::server::read_bytes_sync;
 use crate::server::Server;
 
 use super::errors::ReplError;
@@ -22,14 +23,14 @@ fn expected_response(expected: &str, actual: &[u8]) -> R<()> {
     }
 }
 
-fn do_slave_ping(stream: &mut TcpStream) {
+fn do_slave_ping(stream: &mut TcpStream) -> R<()> {
     let ping = Serializer::to_arr(Vec::from(["ping"]));
     stream.write_all(ping.as_bytes()).expect("Failed to write!");
-    // let ping_resp = read_bytes_sync(stream);
-    // expected_response("ping", &ping_resp)
+    let ping_resp = read_bytes_sync(stream);
+    expected_response("ping", &ping_resp)
 }
 
-fn do_slave_listen(stream: &mut TcpStream, server: &Server) {
+fn do_slave_listen(stream: &mut TcpStream, server: &Server) -> R<()> {
     let listen = Serializer::to_arr(Vec::from([
         "REPLCONF",
         "listening-port",
@@ -38,17 +39,17 @@ fn do_slave_listen(stream: &mut TcpStream, server: &Server) {
     stream
         .write_all(listen.as_bytes())
         .expect("Failed to write!");
-    // let listen_resp = read_bytes_sync(stream);
-    // expected_response("ok", &listen_resp)
+    let listen_resp = read_bytes_sync(stream);
+    expected_response("ok", &listen_resp)
 }
 
-fn do_slave_psync(stream: &mut TcpStream) {
+fn do_slave_psync(stream: &mut TcpStream) -> R<()> {
     let psync = Serializer::to_arr(Vec::from(["REPLCONF", "capa", "psync2"]));
     stream
         .write_all(psync.as_bytes())
         .expect("Failed to write!");
-    // let listen_resp = read_bytes_sync(stream);
-    // expected_response("ok", &listen_resp)
+    let listen_resp = read_bytes_sync(stream);
+    expected_response("ok", &listen_resp)
 }
 
 pub fn do_slave_handshake(server: &Server) -> R<()> {
@@ -58,9 +59,9 @@ pub fn do_slave_handshake(server: &Server) -> R<()> {
     debug_assert!(server.master_addr().is_some());
     match TcpStream::connect(server.master_addr().unwrap()) {
         Ok(mut master_stream) => {
-            //        do_slave_ping(&mut master_stream)?;
-            //        do_slave_listen(&mut master_stream, &server)?;
-            //        do_slave_psync(&mut master_stream)?;
+            do_slave_ping(&mut master_stream)?;
+            do_slave_listen(&mut master_stream, &server)?;
+            do_slave_psync(&mut master_stream)?;
             Ok(())
         }
         Err(_) => Err(ReplError::FailedToConnect),
