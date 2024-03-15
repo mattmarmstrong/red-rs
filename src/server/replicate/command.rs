b24d37c3f61fd38d4645e19628d97bb3c24b2e83
Matthew Armstrong <matthew.armstrong@andgosystems.com>
2024-03-15 14:40:37 -0600

    handshake should NOT be async lmao

--- src/server/replicate/command.rs
+++ src/server/replicate/command.rs
@@ -1,5 +1,5 @@
-use tokio::io::AsyncWriteExt;
-use tokio::net::TcpStream;
+use std::io::Write;
+use std::net::TcpStream;
 
 use crate::resp::serialize::Serializer;
 use crate::server::errors::CommandError;
@@ -10,17 +10,16 @@ use super::info::Role;
 // Put code to communicate between replicas here
 type R<T> = anyhow::Result<T, CommandError>;
 
-pub async fn do_repl_handshake(server: &Server) -> R<()> {
+pub fn do_repl_handshake(server: &Server) -> R<()> {
     // TODO -> The rest of the repl_handshake
     debug_assert!(server.replica_info.role == Role::Slave);
     debug_assert!(server.master_addr().is_some());
-    match TcpStream::connect(server.master_addr().unwrap()).await {
+    match TcpStream::connect(server.master_addr().unwrap()) {
         Ok(mut master_stream) => {
             let ping_str = Serializer::to_bulk_str("ping");
             let ping = Serializer::to_arr(Vec::from([ping_str]));
             master_stream
                 .write_all(ping.as_bytes())
-                .await
                 .expect("Failed to write!");
             Ok(())
         }
