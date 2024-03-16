ff4fc1472ce1432ddc0642888a9aac8a6f225a60
Matthew Armstrong <matthew.armstrong@andgosystems.com>
2024-03-15 18:02:11 -0600

    test

--- src/server/replicate/command.rs
+++ src/server/replicate/command.rs
@@ -1,14 +1,60 @@
 use std::io::Write;
 use std::net::TcpStream;
 
+use crate::resp::parse::Parser;
 use crate::resp::serialize::Serializer;
-use crate::server::errors::CommandError;
 use crate::server::Server;
 
+use super::errors::ReplError;
 use super::info::Role;
 
 // Put code to communicate between replicas here
-type R<T> = anyhow::Result<T, CommandError>;
+type R<T> = anyhow::Result<T, ReplError>;
+
+fn expected_response(expected: &str, actual: &[u8]) -> R<()> {
+    if let Ok(resp) = Parser::new(actual).parse() {
+        match resp.cmp_str(expected) {
+            true => Ok(()),
+            false => Err(ReplError::UnexpectedResponse),
+        }
+    } else {
+        Err(ReplError::InvalidResponse)
+    }
+}
+
+// async fn do_slave_ping(stream: &mut TcpStream) -> R<()> {
+//     let ping = Serializer::to_arr(Vec::from(["ping"]));
+//     stream
+//         .write_all(ping.as_bytes())
+//         .await
+//         .expect("Failed to write!");
+//     let ping_resp = read_bytes(stream).await;
+//     expected_response("ping", &ping_resp)
+// }
+//
+// async fn do_slave_listen(stream: &mut TcpStream, server: &Server) -> R<()> {
+//     let listen = Serializer::to_arr(Vec::from([
+//         "REPLCONF",
+//         "listening-port",
+//         &server.port.to_string(),
+//     ]));
+//     stream
+//         .write_all(listen.as_bytes())
+//         .await
+//         .expect("Failed to write!");
+//     let listen_resp = read_bytes(stream).await;
+//     expected_response("ok", &listen_resp)
+// }
+//
+// async fn do_slave_psync(stream: &mut TcpStream) -> R<()> {
+//     let psync = Serializer::to_arr(Vec::from(["REPLCONF", "capa", "psync2"]));
+//     stream
+//         .write_all(psync.as_bytes())
+//         .await
+//         .expect("Failed to write!");
+//     let listen_resp = read_bytes(stream).await;
+//     expected_response("ok", &listen_resp)
+// }
 
 pub fn do_repl_handshake(server: &Server) -> R<()> {
     // TODO -> The rest of the repl_handshake
@@ -16,14 +62,13 @@ pub fn do_repl_handshake(server: &Server) -> R<()> {
     debug_assert!(server.master_addr().is_some());
     match TcpStream::connect(server.master_addr().unwrap()) {
         Ok(mut master_stream) => {
-            let ping_str = Serializer::to_bulk_str("ping");
-            let ping = Serializer::to_arr(Vec::from([ping_str]));
+            let ping = Serializer::to_arr(Vec::from(["ping"]));
             println!("{}", ping);
             master_stream
                 .write_all(ping.as_bytes())
                 .expect("Failed to write!");
             Ok(())
         }
-        Err(_) => Err(CommandError::CommandFailed),
+        Err(_) => Err(ReplError::CommandFailed),
     }
 }
