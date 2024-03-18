3094ce57b6946b40e9dc38d67375e7f1fb01d6a5
Matthew Armstrong <matthew.armstrong@andgosystems.com>
2024-03-18 15:46:41 -0600

    jank

--- src/server/connect.rs
+++ src/server/connect.rs
@@ -6,36 +6,24 @@ use crate::resp::parse::Parser;
 
 type R<T> = anyhow::Result<T>;
 
-pub struct Connection {
-    stream: TcpStream,
+pub async fn read(stream: &mut TcpStream) -> R<Option<DataType>> {
+    let mut buffer = [0u8; 1024];
+    loop {
+        let bytes_read = stream.read(&mut buffer).await.expect("Read failed!");
+        if bytes_read != 0 {
+            break;
+        }
+    }
+
+    let parsed_data = Parser::new(&mut buffer).parse().ok();
+    Ok(parsed_data)
 }
 
-impl Connection {
-    pub fn new(stream: TcpStream) -> Self {
-        Self { stream }
-    }
-
-    pub async fn read(&mut self) -> R<Option<DataType>> {
-        let mut buffer = [0u8; 1024];
-        loop {
-            let bytes_read = self.stream.read(&mut buffer).await.expect("Read failed!");
-            println!("Bytes read: {}", bytes_read);
-            if bytes_read != 0 {
-                break;
-            }
-        }
-
-        let parsed_data = Parser::new(&mut buffer).parse().ok();
-        Ok(parsed_data)
-    }
-
-    pub async fn write(&mut self, msg: String) -> R<()> {
-        println!("WRITING! MSG: {}", msg);
-        self.stream
-            .write_all(msg.as_bytes())
-            .await
-            .expect("Write failed!");
-        self.stream.flush().await.expect("Flush failed!");
-        Ok(())
-    }
+pub async fn write(stream: &mut TcpStream, msg: String) -> R<()> {
+    stream
+        .write_all(msg.as_bytes())
+        .await
+        .expect("Write failed!");
+    stream.flush().await.expect("Flush failed!");
+    Ok(())
 }
--- src/server/replicate/command.rs
+++ src/server/replicate/command.rs
@@ -1,48 +1,55 @@
 use tokio::net::TcpStream;
 
 use crate::resp::serialize::Serializer;
-use crate::server::connect::Connection;
+use crate::server::connect::{read, write};
 use crate::server::Server;
 
 use super::errors::ReplError;
 
 type R<T> = anyhow::Result<T, ReplError>;
 
-async fn do_follower_ping(c: &mut Connection) -> R<()> {
+async fn do_follower_ping(s: &mut TcpStream) -> R<()> {
     let ping = Serializer::to_arr(Vec::from(["ping"]));
-    c.write(ping).await.expect("Write failed!");
-    let ping_resp = c.read().await.expect("Read failed!");
+    write(s, ping).await.expect("Write failed!");
+    let ping_resp = read(s).await.expect("Read failed!");
     assert!(ping_resp.unwrap().cmp_str("pong"));
     Ok(())
 }
 
-async fn do_follower_listen(c: &mut Connection, server: &Server) -> R<()> {
+async fn do_follower_listen(s: &mut TcpStream, server: &Server) -> R<()> {
     let listen = Serializer::to_arr(Vec::from([
         "REPLCONF",
         "listening-port",
         &server.port.to_string(),
     ]));
-    c.write(listen).await.expect("Write failed!");
-    let listen_resp = c.read().await.expect("Read failed!");
+    write(s, listen).await.expect("Write failed!");
+    let listen_resp = read(s).await.expect("Read failed!");
     assert!(listen_resp.unwrap().cmp_str("ok"));
     Ok(())
 }
-async fn do_follower_psync(c: &mut Connection) -> R<()> {
-    let psync = Serializer::to_arr(Vec::from(["REPLCONF", "capa", "psync2"]));
-    c.write(psync).await.expect("Write failed!");
-    let psync_resp = c.read().await.expect("Read failed!");
-    assert!(psync_resp.unwrap().cmp_str("ok"));
+async fn do_follower_capa(s: &mut TcpStream) -> R<()> {
+    let capa = Serializer::to_arr(Vec::from(["REPLCONF", "capa", "psync2"]));
+    write(s, capa).await.expect("Write failed!");
+    let capa_resp = read(s).await.expect("Read failed!");
+    assert!(capa_resp.unwrap().cmp_str("ok"));
     Ok(())
 }
 
 pub async fn do_repl_handshake(server: &Server) -> R<()> {
     // TODO -> The rest of the repl_handshake
-    let stream = TcpStream::connect(server.master_addr().unwrap())
+    let mut stream = TcpStream::connect(server.master_addr().unwrap())
         .await
         .expect("Failed to connect!");
-    let mut connect = Connection::new(stream);
-    do_follower_ping(&mut connect).await?;
-    do_follower_listen(&mut connect, server).await?;
-    do_follower_psync(&mut connect).await?;
+    do_follower_ping(&mut stream).await?;
+    do_follower_listen(&mut stream, server).await?;
+    do_follower_capa(&mut stream).await?;
+    do_psync(&mut stream, server).await?;
+    Ok(())
+}
+
+async fn do_psync(s: &mut TcpStream, _server: &Server) -> R<()> {
+    let psync = Serializer::to_arr(Vec::from(["PSYNC", "?", "-1"]));
+    write(s, psync).await.expect("Write failed!");
+    let _psync_resp = read(s).await.expect("Read failed!");
     Ok(())
 }
