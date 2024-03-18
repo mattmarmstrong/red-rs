a6121c10c5ef741cc87aee5646a4a980f074d1db
Matthew Armstrong <matthew.armstrong@andgosystems.com>
2024-03-18 14:31:55 -0600

    ??

--- src/server/replicate/command.rs
+++ src/server/replicate/command.rs
@@ -20,42 +20,30 @@ fn expected_response(expected: &str, actual: &[u8]) -> R<()> {
         Err(ReplError::InvalidResponse)
     }
 }
-
-async fn do_follower_ping(c: &mut Connection) -> R<()> {
-    let ping = Serializer::to_arr(Vec::from(["ping"]));
-    c.write(ping).await.expect("Write failed!");
-    c.read().await.expect("Read failed!");
-    expected_response("ping", &mut c.buffer)
-}
-
-async fn do_follower_listen(c: &mut Connection, server: &Server) -> R<()> {
-    let listen = Serializer::to_arr(Vec::from([
-        "REPLCONF",
-        "listening-port",
-        &server.port.to_string(),
-    ]));
-    c.write(listen).await.expect("Write failed!");
-    c.read().await.expect("Read failed!");
-    expected_response("ok", &mut c.buffer)
-}
-async fn do_follower_psync(c: &mut Connection) -> R<()> {
-    let psync = Serializer::to_arr(Vec::from(["REPLCONF", "capa", "psync2"]));
-    c.write(psync).await.expect("Write failed!");
-    c.read().await.expect("Read failed!");
-    expected_response("ok", &mut c.buffer)
-}
-
 pub async fn do_repl_handshake(server: &Server) -> R<()> {
     // TODO -> The rest of the repl_handshake
     let stream = TcpStream::connect(server.master_addr().unwrap())
         .await
         .expect("Failed to connect!");
     let mut connect = Connection::new(stream);
-    do_follower_ping(&mut connect).await?;
+    let ping = Serializer::to_arr(Vec::from(["ping"]));
+    connect.write(ping).await.expect("Write failed!");
+    connect.read().await.expect("Read failed!");
+    expected_response("ping", &mut connect.buffer)?;
     connect.buf_clear().unwrap();
-    do_follower_listen(&mut connect, server).await?;
+    let listen = Serializer::to_arr(Vec::from([
+        "REPLCONF",
+        "listening-port",
+        &server.port.to_string(),
+    ]));
+    connect.write(listen).await.expect("Write failed!");
+    connect.read().await.expect("Read failed!");
+    expected_response("ok", &mut connect.buffer)?;
     connect.buf_clear().unwrap();
-    do_follower_psync(&mut connect).await?;
+    let psync = Serializer::to_arr(Vec::from(["REPLCONF", "capa", "psync2"]));
+    connect.write(psync).await.expect("Write failed!");
+    connect.read().await.expect("Read failed!");
+    expected_response("ok", &mut connect.buffer)?;
     connect.buf_clear().unwrap();
     Ok(())
 }
