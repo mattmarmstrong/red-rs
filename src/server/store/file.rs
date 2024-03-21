472eb0b082e613470129eaed0f805bdf6db28a12
Matthew Armstrong <matthew.armstrong@andgosystems.com>
2024-03-20 22:04:13 -0600

    sparks flying

--- src/server/store/file.rs
+++ src/server/store/file.rs
@@ -1,17 +1,20 @@
 use std::num::ParseIntError;
 
-pub const EMPTY: &str = "
-524544495330303131fa0972656469732
-d76657205372e322e30fa0a7265646973
-2d62697473c040fa056374696d65c26d0
-8bc65fa08757365642d6d656dc2b0c410
-00fa08616f662d62617365c000fff06e3
-bfec0ff5aa2";
+pub const EMPTY: &[u8] = b"\
+524544495330303131fa097265646973\
+2d76657205372e322e30fa0a72656469\
+732d62697473c040fa056374696d65c2\
+6d08bc65fa08757365642d6d656dc2b0\
+c41000fa08616f662d62617365c000ff\
+f06e3bfec0ff5aa2";
 
-fn hex_to_bytes(hex: &str) -> Result<Vec<u8>, ParseIntError> {
+fn hex_to_bytes(hex: &[u8]) -> Result<Vec<u8>, ParseIntError> {
     (0..hex.len())
         .step_by(2)
-        .map(|i| u8::from_str_radix(&hex[i..i + 2], 16))
+        .map(|i| {
+            let str = std::str::from_utf8(&hex[i..i + 2]).unwrap();
+            u8::from_str_radix(str, 16)
+        })
         .collect()
 }
 
